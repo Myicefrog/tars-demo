3aea60011c62a3dbf6cd196506fee08db496956f
Myicefrog <490925498@qq.com>
2018-11-12 23:53:26 -0800

    save

--- tc_epoll_server.cpp
+++ tc_epoll_server.cpp
@@ -1,5 +1,19 @@
 #include "tc_epoll_server.h"
 
+#include <unistd.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <cerrno>
+#include <cassert>
+#include <ifaddrs.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <net/if_arp.h>
+#include <netinet/tcp.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
 using namespace std;
 
 namespace tars
@@ -27,7 +41,82 @@ TC_EpollServer::NetThread::NetThread(TC_EpollServer *epollServer)
        	int iDomain = AF_INET;
 	_shutdown_sock = socket(iDomain, iSocketType, 0);
 	_notify_sock = socket(iDomain, iSocketType, 0);
+	
+	if(_shutdown_sock < 0)
+	{
+		cout<<"_shutdown_sock  invalid"<<endl;
+	}
+	if(_notify_sock < 0)
+	{
+		cout<<"_notify_sock invalid"<<endl;
+	}
 }
+
+TC_EpollServer::NetThread::~NetThread()
+{
+}
+
+int  TC_EpollServer::NetThread::bind(string& ip, int& port)
+{
+	int iSocketType = SOCK_STREAM;
+	int iDomain = AF_INET;
+	_sock = socket(iDomain, iSocketType, 0);
+	
+	if(_sock < 0)
+	{
+		cout<<"bind _sock invalid"<<endl;
+	}
+
+	struct sockaddr_in bindAddr;
+	
+	bzero(&bindAddr, sizeof(bindAddr));
+	
+	bindAddr.sin_family   = iDomain;
+	bindAddr.sin_port     = htons(port);
+
+	parseAddr(sServerAddr, bindAddr.sin_addr);	
+
+	//如果服务器终止后,服务器可以第二次快速启动而不用等待一段时间
+	int iReuseAddr = 1;
+
+	setSockOpt(SO_REUSEADDR, (const void *)&iReuseAddr, sizeof(int), SOL_SOCKET);
+	
+	if(::bind(_sock, pstBindAddr, iAddrLen) < 0)
+	{
+		cout<<"bind error"<<endl;
+	}
+	
+	return _sock;
+}
+
+void TC_EpollServer::NetThread::parseAddr(const string &sAddr, struct in_addr &stSinAddr)
+{
+	//将点分十进制转为二进制整数
+	int iRet = inet_pton(AF_INET, sAddr.c_str(), &stSinAddr);
+	if(iRet < 0)
+	{
+		cout<<"parseAddr iRet error"<<endl;
+	}
+	else if(iRet == 0)
+	{
+		struct hostent stHostent;
+		struct hostent *pstHostent;
+		char buf[2048] = "\0";
+		int iError;
+
+		gethostbyname_r(sAddr.c_str(), &stHostent, buf, sizeof(buf), &pstHostent, &iError);
+
+		if (pstHostent == NULL)
+		{
+			cout<<"gethostbyname_r error! :"<<endl;
+		}
+		else
+		{
+			stSinAddr = *(struct in_addr *) pstHostent->h_addr;
+		}	
+	}
+}
+
 };
 
 }
--- tc_epoll_server.h
+++ tc_epoll_server.h
@@ -1,6 +1,10 @@
 #ifndef __TARS_TC_EPOLL_SERVER_H_
 #define __TARS_TC_EPOLL_SERVER_H_
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -20,10 +24,13 @@ public:
 	public:
 		NetThread(TC_EpollServer *epollServer);
 		virtual ~NetThread();	
+		int bind(string& ip, int& port);
+		static void parseAddr(const string &sAddr, struct in_addr &stAddr);
 	private:
 		TC_EpollServer            *_epollServer;
 		int _shutdown_sock;
 		int _notify_sock;
+		int _sock;
 	};
 private:
 	std::vector<NetThread*>        _netThreads;
