be66a3dceb0d26e9e4941a75c1e39fb4751f71a6
Myicefrog <490925498@qq.com>
2018-11-14 17:13:08 -0800

    add client

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main()
 
 	TC_EpollServer  _epollServer(1);
 	vector<TC_EpollServer::NetThread*> vNetThread = _epollServer.getNetThread();
-	string ip = "192.168.128.139";
+	string ip = "127.0.0.1";
 	int port = 9877;
 	vNetThread[0]->bind(ip,port);
 	vNetThread[0]->run();
